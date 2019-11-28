28791293fb4f6dadc180f3021c48d64061aee562
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-28 17:39:07 -0300

    Guia0Cadenas1

---
+++ Guia0Cadenas1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia0Cadenas1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Ingrese nombre");
+			var nombre = Console.In.ReadLine();
+			Console.WriteLine("Hola " + nombre);
+			Console.ReadKey();
+		}
+	}
+}
