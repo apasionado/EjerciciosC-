41ca4fba7fb7f77f59c56096e4866e23f71ad29e
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-28 16:05:02 -0300

    Guia0Ejercicio3

---
+++ Guia0Consola3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia0Consola3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(Console.ReadKey().Key != ConsoleKey.X){
+                
+
+            }
+            Console.Out.WriteLine("Salio bien");
+            Console.ReadKey();
+        }
+    }
+}
