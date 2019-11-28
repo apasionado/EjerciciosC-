2aa604450247c25bc0ecf67d10f6f12867f54780
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-28 18:02:53 -0300

    Correcciones

--- Guia0Cadenas2/Program.cs
+++ Guia0Cadenas2/Program.cs
@@ -10,6 +10,17 @@ namespace Guia0Cadenas2
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Ingrese una frase que tenga \"Fin\" ");
+			var frase = Console.In.ReadLine();
+			if (frase.Contains("fin"))
+			{
+				Console.WriteLine("Tiene \"Fin\" ");
+			}
+			else
+			{
+				Console.WriteLine("No contiene \"Fin\" ");
+			}
+			Console.ReadKey();
 		}
 	}
 }
--- Guia0Cadenas4/Program.cs
+++ Guia0Cadenas4/Program.cs
@@ -2,19 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Guia0Cadenas1
+namespace Guia0Cadenas4
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Ingrese nombre");
-			nombre = Console.ReadLine();
-			Console.Write("Hola "+ nombre);
-			Console.ReadKey();
+
 		}
 	}
 }
