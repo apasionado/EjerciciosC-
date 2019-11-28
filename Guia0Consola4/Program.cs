db81032c868cfd793498b67b67e07b0028f600d7
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-28 16:07:01 -0300

    Guia0Ejercicio4

---
+++ Guia0Consola4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia0Consola4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "fin")
+            {
+
+            }
+        }
+    }
+}
