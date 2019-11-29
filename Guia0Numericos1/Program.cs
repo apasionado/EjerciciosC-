85a2d580d570eaf12fefa8b1f4ac854a1796046b
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-29 11:08:35 -0300

    Quiero hacer cosas del chatbot

---
+++ Guia0Numericos1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Guia0Numericos1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//15.	Solicite un ingreso al usuario y determine si puede ser interpretado como un valor numérico.
+			var ingreso = Console.ReadLine();
+			Regex numeros = new Regex(@"^\d+$");
+			if (numeros.IsMatch(ingreso))
+			{
+				Console.Write("Determino que se interpreta el valor ingresado como un valor numerico");
+			}
+			else
+			{
+				Console.Write("No puede ser interpretado como un valor numerico");
+			}
+			Console.ReadKey();
+		}
+	}
+}
