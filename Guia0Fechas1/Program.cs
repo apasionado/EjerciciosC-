c33ccd9de22bee2f2e1b18f2395c5ad8bb658430
Fernando <35930141+apasionado@users.noreply.github.com>
2019-11-29 16:02:46 -0300

    Fecha actual

---
+++ Guia0Fechas1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia0Fechas1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//21. Presente al usuario la fecha actual.
+			Console.WriteLine("La fecha actual es: " + System.DateTime.Now.ToString());
+			Console.ReadKey();
+
+		}
+	}
+}
