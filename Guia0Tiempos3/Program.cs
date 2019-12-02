f5722b6844296f8fe6858e104d22b3de9a9fc540
Fernando <35930141+apasionado@users.noreply.github.com>
2019-12-02 17:06:49 -0300

    Mas ejercicios

---
+++ Guia0Tiempos3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Guia0Tiempos3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//3Solicite el ingreso de una fecha al usuario y determine cuántos días restan para el fin de año próximo más cercano.
+			bool exito = false;
+			while (exito != true)
+			{
+				Console.WriteLine("Ingrese una fecha");
+				String fechaIngresada = Console.ReadLine();
+				Regex patron = new Regex(@"^(0?[1-9]|[12]\d|3[01])[\/](0?[1-9]|1[0-2])[\/](19|20)\d{2}$");
+				if (patron.IsMatch(fechaIngresada))
+				{
+					DateTime fecha = DateTime.ParseExact(fechaIngresada, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+					var anioSiguiente = fecha.Year + 1;
+					String finDeAnio = "01/01/" + anioSiguiente;
+					DateTime fechaFinDeAnio = DateTime.ParseExact(finDeAnio, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+					exito = true;
+
+					var faltaParaFinDeAnio = (fecha - fechaFinDeAnio).TotalDays;
+					//redondeo
+					faltaParaFinDeAnio = Math.Abs(faltaParaFinDeAnio);
+					Console.WriteLine("Falta para el fin de anio: " + faltaParaFinDeAnio);
+
+				}
+				else
+				{
+					Console.WriteLine("Ingrese una fecha valida");
+				}
+				Console.ReadKey();
+			}
+		}
+	}
+}
---
+++ Guia0Tiempos4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Guia0Tiempos4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//4Presente la fecha actual cada 10 segundos durante un minuto.
+
+			Console.WriteLine("Te voy a mostrar la fecha actual cada 10 segundos durante 1 minuto");
+
+			var quiero = DateTime.Now.AddSeconds(60);
+			var estimado = DateTime.Now.AddSeconds(0);
+
+			Console.WriteLine(quiero + " Quiero");
+			Console.WriteLine(estimado + " Estimado");
+
+			while (DateTime.Now != quiero)
+			{
+				if (estimado.Second == DateTime.Now.Second)
+				{
+					estimado = DateTime.Now.AddSeconds(10);
+					Console.WriteLine(estimado + " La nueva fecha esperada es esta ");
+				}
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
