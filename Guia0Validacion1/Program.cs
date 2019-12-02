0b43d403aa5f4d5874163aad4518053e6be93143
Fernando <35930141+apasionado@users.noreply.github.com>
2019-12-02 17:33:02 -0300

    Mas ejercicios

---
+++ Guia0Validacion1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Guia0Validacion1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//1Solicitar el ingreso de un número al usuario, repitiendo la operación (presentando un mensaje de error) hasta que el ingreso sea válido.
+			Console.WriteLine("Ingrese un número");
+			String ingreso = Console.ReadLine();
+			Regex patron = new Regex(@"\d+");
+			while (!patron.IsMatch(ingreso))
+			{
+				Console.WriteLine("ERROR: El ingreso no fue exitoso, por favor ingrese un numero");
+				ingreso = Console.ReadLine();
+			}
+			Console.ReadKey();
+		}
+	}
+}
