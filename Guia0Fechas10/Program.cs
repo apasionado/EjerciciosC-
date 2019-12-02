03132f0884b4a123d9c904e99288915cfd8e85c1
Fernando <35930141+apasionado@users.noreply.github.com>
2019-12-02 11:05:05 -0300

    ejercicio 10

---
+++ Guia0Fechas10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Guia0Fechas10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Solicite el ingreso de una fecha al usuario y muestre sólo la hora.
+			bool exito = false;
+			while (exito != true)
+			{
+				Console.WriteLine("Ingrese una fecha");
+				String fechaIngresada = Console.ReadLine();
+				Regex patron = new Regex(@"(?m)/(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}( \d{1,2}[:-]\d{2}([:-]\d{2,3})*)?");
+				if (patron.IsMatch(fechaIngresada))
+				{
+					DateTime fecha = DateTime.ParseExact(fechaIngresada, "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+					exito = true;
+					Console.WriteLine("La hora de la fecha ingresada es: ");
+					var horaDeLaFecha = fecha.TimeOfDay.ToString();
+				}
+				else
+				{
+					Console.WriteLine("Ingrese una fecha valida");
+				}
+				Console.ReadKey();
+			}
+		}
+	}
+}
