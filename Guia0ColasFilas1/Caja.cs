7ae66a85eca64d05d82bf2df6d6b8f6bc2aa54c7
Fernando <35930141+apasionado@users.noreply.github.com>
2019-12-04 11:45:41 -0300

    Ejercicio

---
+++ Guia0ColasFilas1/Caja.cs
@@ -0,0 +1,16 @@
+﻿namespace Guia0ColasFilas1
+{
+    internal class Caja
+    {
+
+        public bool estaOcupada;
+        public string afiliadoAtendido;
+
+
+        public string asignarAlAfiliado(string afiliado)
+        {
+
+        }
+
+    }
+}
--- Guia0ColasFilas1/Program.cs
+++ Guia0ColasFilas1/Program.cs
@@ -25,49 +25,94 @@ b.	El estado de cada caja (“libre” o el nombre del afiliado que está siendo
             Console.WriteLine("Ingrese un comando afiliado <nombre>, caja <numero>,  fin");
             List<String> lista = new List<String>();
             bool terminar = false;
-            while (terminar != true)
+
+            String comando;
+            Regex comandoConNumero = new Regex(@"^caja\s?\d{0,3}$");
+            Regex comandoSolo = new Regex(@"^\w+\b$");
+            // Se puede pasar a Do While, el primer paso lo hace siempre y luego chequea la condicion
+            Queue<string> afiliadosEnEspera = new Queue<string>();
+            // Cajas que pueden ponerse en cola
+            Queue<int> cajasDisponibles = new Queue<int>();
+            // Cada caja individual, con su afiliado (o, un array de cajas y acceder con el indice o foreach)
+            Queue<string> caja1 = new Queue<string>();
+            Queue<string> caja2 = new Queue<string>();
+            Queue<string> caja3 = new Queue<string>();
+            do
             {
-                String comando = Console.ReadLine();
-                Regex comandoConNumero = new Regex(@"^caja\s?\d{0,3}$");
-                Regex comandoConLetra = new Regex(@"^afiliado\s?\w{0,14}$");
-                Regex comandoSolo = new Regex(@"^fin$");
+                comando = Console.ReadLine();
 
-                while (!(comandoConNumero.IsMatch(comando) || comandoConLetra.IsMatch(comando) || comandoSolo.IsMatch(comando)))
+                string cadenaEncontrada = comandoSolo.Match(comando).Value; // Al ser un solo valor debería funcionar, sino hay que entrar a .groups
+                                                                            //tambien hay un metodo de regex que te devuelve un array, ese también es más facil de manejar, de última usamos ese (creo que es el que vos decis, groups)
+
+                switch (cadenaEncontrada)
                 {
+                    case "caja":
+                        // ejemplo: "caja 1" --> caja1.estaOcupada = false;
 
-                    Console.WriteLine("comando invalido");
-                    comando = Console.ReadLine();
-                }
 
-                if (comandoConNumero.IsMatch(comando))
-                {
-                    comando = comando.Replace("caja ", "");
 
-                }
-                if (comandoConLetra.IsMatch(comando))
-                {
-                    if (comando.Contains("alta"))
-                    {
-                        comando = comando.Replace("alta ", "");
-                        Console.WriteLine("Se agregará a la lista el nombre: " + comando + " despues de la posición " + lista.Count);
+                        /*Luego de procesar cada comando el sistema mostrará:
+        a.	La lista de afiliados en espera, según orden de atención.
+        b.	El estado de cada caja (“libre” o el nombre del afiliado que está siendo atendido).*/
 
-                    }
-                }
+                        foreach (Caja caja in cajasDisponibles)
+                        {
+                            if (caja.estaOcupada == true)
+                            {
+                                Console.WriteLine("La caja: " + caja + " está ocupada");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La caja: " + caja + "está disponible");
+                                caja.asignarAlAfiliado(afiliado);
+                            }
+                        }
+                        break;
 
-                switch (comando)
-                {
 
+                    case "afiliado":
+                        // Agregar al queue "afiliadosEnEspera"
+                        String nombreAfiliado = cadenaEncontrada.replace("afiliado ", "");
+                        afiliadosEnEspera.add(nombreAfiliado);
+
+
+
+
+
+                        /*Luego de procesar cada comando el sistema mostrará:
+    a.	La lista de afiliados en espera, según orden de atención.
+    b.	El estado de cada caja (“libre” o el nombre del afiliado que está siendo atendido).*/
+                        foreach (Caja caja in cajasDisponibles)
+                        {
+                            if (caja.estaOcupada == true)
+                            {
+                                Console.WriteLine("La caja: " + caja + " está ocupada");
+                            }
+                            else
+                            {
+                                Console.WriteLine("La caja: " + caja + "está disponible");
+                                caja.asignarAlCliente(cliente);
+                            }
+                        }
+
+                        break;
                     case "fin":
-
                         Console.WriteLine("Fin del programa");
                         terminar = true;
                         break;
+                    default:
+                        Console.WriteLine("Comando inválido, intente nuevamente");
+                        break;
                 }
+                // Mostrar estado de cajas / filas / pilas / stacks / comosea
 
-            }
+
+
+
+
+            } while (!terminar);
             Console.ReadKey();
         }
 
     }
-}
 }
