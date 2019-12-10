a13a77ac5e007834193e797d3cb46110ccfd60d1
Fernando <35930141+apasionado@users.noreply.github.com>
2019-12-10 11:19:37 -0300

    Ejercicio

---
+++ Guia0Fechas4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Guia0Fechas4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //24.	Solicite el ingreso de una fecha al usuario utilizando, estrictamente, el formato dd/MM/yyyy
+
+
+            bool exito = false;
+            while (exito != true)
+            {
+                Console.WriteLine("Ingrese una fecha");
+                String fechaIngresada = Console.ReadLine();
+                Regex patron = new Regex(@"^(0[1-9]|[12]\d|3[01])[\/](0[1-9]|1[0-2])[\/](19|20)\d{2}$");
+                if (patron.IsMatch(fechaIngresada))
+                {
+                    DateTime fecha = DateTime.ParseExact(fechaIngresada, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                    exito = true;
+                    Console.WriteLine("El mes ingresado es: " + fechaIngresada);
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese una fecha valida");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Guia0Fechas5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia0Fechas5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 5.	Le solicite al usuario el ingreso de tres números y determine 
+             * si conforman una fecha válida interpretados como día / mes / año.
+             */
+
+            Console.WriteLine("Ingrese tres numeros");
+            var fechaParaComprobar = Convert.ToInt32(Console.ReadLine());
+
+            if (fechaParaComprobar > 0)
+            {
+
+            }
+
+
+        }
+    }
+}
